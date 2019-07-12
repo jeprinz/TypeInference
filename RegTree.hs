f0fb017b45eeb09206f552b0d1e412243adbed99
Jacob Prinz <jacobeliasprinz@gmail.com>
2019-07-12 10:48:20 -0400

    Figured some stuff out, using product alg

--- RegTree.hs
+++ RegTree.hs
@@ -5,8 +5,9 @@ module RegTree(
 import Data.Set as Set
 -- import Data.BiMap as BiMap
 import Data.Map as Map
+import Control.Monad.State
 
-type Id = Int
+data Id = Id Int | Idx Id Id deriving (Show, Eq, Ord)
 data RegTree = Mu Id RegTree RegTree | Var Id deriving (Show)
 
 replace :: RegTree -> Id -> RegTree -> RegTree -- replace t v x = t[x/v]
@@ -24,27 +25,41 @@ right (Mu topVar t1 t2) = left (Mu topVar t2 t1)
 
 type Substitutions = Map Id RegTree
 
-intersect :: RegTree -> RegTree -> (RegTree, Substitutions)
-intersect = intersectImpl Map.empty
+intersectI :: Substitutions -> RegTree -> RegTree -> (RegTree, Substitutions)
+intersectI boundVars (Mu x a1 b1) (Mu y a2 b2) = case Map.lookup (Idx x y) boundVars of
+                         Just subst -> (Var (Idx x y), boundVars)
+                         Nothing -> let (a, subs) =  intersectI boundVars a1 a2
+                                        (b, subs') = (intersectI subs b1 b2)
+                                    in (Mu (Idx x y) a b, insert (Idx x y )subs')
 
+-- intersectI :: RegTree -> RegTree -> State Substitutions RegTree
+-- intersectI (Mu x a1 b1) (Mu y a2 b2) =
+--                          case Map.lookup (Idx x y) get of
+--                             Just subst -> do return $ Var (Idx x y)
+--                             Nothing -> do a <- intersectI a1 a2
+--                                           b <- intersectI b1 b2
+--                                           put $ Map.empty
+--                                           return $ Mu (Idx x y) a b
 
+-- intersect :: RegTree -> RegTree -> (RegTree, Substitutions)
+-- intersect = intersectImpl Map.empty
+--
+-- addToMap :: Substitutions -> Id -> Id -> Substitutions
+-- addToMap ss v1 v2 = if v1 < v2
+--                          then Map.insert v1 (Var v2) ss
+--                          else Map.insert v2 (Var v1) ss
+--
+-- --               bound variables  tree1      tree2       result   variables that are set to things
+-- intersectImpl :: Substitutions -> RegTree -> RegTree -> (RegTree, Substitutions)
+-- intersectImpl boundVars (Var v1) (Var v2) =
+--                          case Map.lookup v1 boundVars of
+--                               Just subst -> undefined
+--                               Nothing -> case Map.lookup v2 boundVars of
+--                                               Just subst -> undefined
+--                                               Nothing -> (Var (min v1 v2), addToMap Map.empty v1 v2)
 
-addToMap :: Substitutions -> Id -> Id -> Substitutions
-addToMap ss v1 v2 = if v1 < v2
-                         then Map.insert v1 (Var v2) ss
-                         else Map.insert v2 (Var v1) ss
-
---               bound variables  tree1      tree2       result   variables that are set to things
-intersectImpl :: Substitutions -> RegTree -> RegTree -> (RegTree, Substitutions)
-intersectImpl boundVars (Var v1) (Var v2) =
-                         case Map.lookup v1 boundVars of
-                              Just subst -> undefined
-                              Nothing -> case Map.lookup v2 boundVars of
-                                              Just subst -> undefined
-                                              Nothing -> (Var (min v1 v2), addToMap Map.empty v1 v2)
-
-example = Mu 0 (Var 0) (Var 0)
+example = Mu (Id 0) (Var (Id 0)) (Var (Id 0))
 -- (A -> A) -> (B -> C)
-example2 = Mu 0 (Mu 1 (Var 2) (Var 2)) (Mu 3 (Var 4) (Var 5))
+example2 = Mu (Id 0) (Mu (Id 1) (Var (Id 2)) (Var (Id 2))) (Mu (Id 3) (Var (Id 4)) (Var (Id 5)))
 -- u A . (B -> A) -> A -- left is u X . B -> (u A . X -> A)
-example3 = Mu 0 (Mu 1 (Var 2) (Var 0)) (Var 0)
+example3 = Mu (Id 0) (Mu (Id 1) (Var (Id 2)) (Var (Id 0))) (Var (Id 0))
